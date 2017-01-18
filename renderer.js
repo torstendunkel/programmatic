cba522e5012102886fa682481e41ceb53ab83304
torstendunkel <torsten.dunkel@tamedia.ch>
2017-01-18 13:14:26 +0100

    changed structure

--- renderer.js
+++ renderer.js
@@ -5,27 +5,33 @@ ch.tam = ch.tam || {};
 ch.tam.addnexusRender = (function(){
 
   var settings = {
-      member : 3646,
-      numads : 3,
-      tagid : 9461257,
-      template : '<div class="singlebox bottom left right"><a target="_blank" href="http://ppn.ch/"><img class="adimage" alt="Werbung" src="{{img}}"/></a><div class="title"> <a target="_blank" href="http://ppn.ch/">{{title}}</a></div><div class="text"><a target="_blank" href="http://ppn.ch/">{{description}}</a></div><div class="url"><a target="_blank" href="http://ppn.ch/"></a></div> </div>'
+      identifier : 'ppn_sb_billboard3_DE_20min', //fallback
+      member : 3646, // fallback variables
+      numads : 4, // fallback variables
+      tagid : 9461257, // fallback variables
+      idPrefix : 'ad-',
+      trackingPixel : '<img class="pixel" src="{{imgSrc}}" width="0" height="0" style="display:none"/>',
+      wrapper : '<div class="{{identifier}}"><div class="ppncaption">Werbung</div><div id="ppninnerbox" class="ppninnerbox">{{content}}<div class="tnlogo" style="position: absolute; top:0; left:0; width:100%;height: 10px;"><a href="https://goo.gl/gJLreW" target="_blank">Anzeige</a></div>',
+      template : '<div class="singlebox bottom left right" id="{{id}}"><a target="_blank" href="{{href}}"><img class="adimage" alt="Werbung" src="{{img}}"/></a><div class="title"> <a target="_blank" href="{{href}}">{{title}}</a></div><div class="text"><a target="_blank" href="{{href}}">{{description}}</a></div><div class="url"><a target="_blank" href="{{href}}"></a></div>{{impression}}</div>'
   };
 
-  var Renderer = function(options){
-
-    this.options = options;
+  var Renderer = function(){
     this.settings = settings;
-
     if(this.validateOptions()){
       this.init();
     }
-
   };
 
   Renderer.prototype = {
 
     init: function(){
       var _this = this;
+
+      this.loadedAds = [];
+      this.adsLoaded = 0;
+
+      this.addStyle();
+
       apntag.anq.push(function() {
         _this.pushTags();
       });
@@ -33,10 +39,17 @@ ch.tam.addnexusRender = (function(){
 
     // validates the options object to check if ads can be requested and rendered
     validateOptions: function(){
+        try{
+            this.options = JSON.parse('{"' + decodeURI(location.hash.substring(1)).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+        }
+        catch(e){
+            this.options = {};
+        }
 
-        this.options.numads = this.options.numads|| this.settings.numads;
-        this.options.tagid = this.options.tagid|| this.settings.tagid;
-        console.log(this.options);
+        this.options.numads = parseInt(this.options.numads) || this.settings.numads;
+        this.options.member = parseInt(this.options.member) || this.settings.member;
+        this.options.identifier = this.options.identifier || this.settings.identifier;
+        this.options.tagid = parseInt(this.options.tagid)    || this.settings.tagid;
         return true;
     },
 
@@ -46,58 +59,128 @@ ch.tam.addnexusRender = (function(){
         member: parseInt(this.options.member) || this.settings.member
       });
 
+
+
       for(var i=0; i< this.options.numads; i++){
         apntag.defineTag({
           tagId: parseInt(this.options.tagid),
           allowedFormats: ['native'],
           allowSmallerSizes : true,
           sizes: [[1,1]],
-          targetId: 'native'+i,
+          targetId: this.settings.idPrefix+i,
           prebid : true
         });
-        apntag.onEvent('adAvailable', 'native'+i, this.adAvailable.bind(this));
+        apntag.onEvent('adAvailable', this.settings.idPrefix+i, this.adAvailable.bind(this,this.settings.idPrefix+i));
       }
       apntag.loadTags();
     },
 
-    adAvailable: function(data){
-      console.log(data);
-      if(data && data.native && !data.nobid){
-        this.renderNativeAd(data.native);
+    adAvailable: function(id,data){
+      this.adsLoaded += 1;
+      data.id = id;
+      this.loadedAds.push(data);
+      //wait for all ads and then render
+      if(this.adsLoaded === this.options.numads){
+         this.render();
       }
+    },
+
+    render: function(){
+        var data= {
+            content : '',
+            identifier : this.options.identifier
+        };
+
+        for(var i=0; i<this.loadedAds.length; i++){
+            data.content += this.renderNativeAd(this.loadedAds[i]);
+        }
+        this.appendToBody(this.tmpl(this.settings.wrapper, data));
+
+        this.initClickTracking();
     },
 
     renderNativeAd: function(data){
         var obj = {
-          title : data.title,
-            img : data.mainMedia[0].url,
-            description : data.description
+          title : data.native.title,
+            img : data.native.mainMedia[0].url,
+            description : data.native.description,
+            href : data.native.clickUrl,
+            impression : '',
+            id: data.id
         };
-        var ad = this.createDomNodeFromHTML(this.tmpl(this.settings.template, obj))[0];
 
-        this.prependNode(ad,document.getElementById("ppninnerbox"));
-
+        //add impression pixels to the native ad
+        if(data.native && data.native.impressionTrackers && data.native.impressionTrackers.length >0){
+           var impressionTracker = data.native.impressionTrackers;
+            for(var i=0; i<impressionTracker.length; i++){
+                obj.impression +=  this.tmpl(this.settings.trackingPixel, {imgSrc: impressionTracker[i]});
+            }
+        }
+        return this.tmpl(this.settings.template, obj);
     },
 
+    initClickTracking: function(){
+        for(var i=0; i<this.loadedAds.length; i++){
+            if(this.loadedAds[i].native && this.loadedAds[i].native.clickTrackers && this.loadedAds[i].native.clickTrackers.length >0){
+                for(var j=0; j<this.loadedAds[i].native.clickTrackers.length; j++){
+                    this.addClickTracking(this.settings.idPrefix + i, this.loadedAds[i].native.clickTrackers[0]);
+                }
+            }
+        }
+    },
+
+    addClickTracking: function(id, trackingUrl){
+        // find all a-tag in the specified ad and add event listener to them
+        var elem = document.getElementById(id);
+        if(elem){
+            this.addEvent(elem,'click',this.trackClick.bind(this,elem,trackingUrl))
+        }
+    },
+
+    trackClick: function(elem,trackingUrl){
+        elem.appendChild(this.createDomNodeFromHTML(this.tmpl(this.settings.trackingPixel,{imgSrc:trackingUrl}))[0]);
+    },
+
+    // ###########################  HELPER  ###########################
+
+    addEvent: function(elem, evnt, func){
+      if (elem.addEventListener) // W3C DOM
+          elem.addEventListener(evnt, func, false);
+      else if (elem.attachEvent) { // IE DOM
+          elem.attachEvent("on" + evnt, func);
+      } else { // No much to do
+          elem[evnt] = func;
+      }
+    },
+
+    appendToBody: function(content){
+      document.body.appendChild(this.createDomNodeFromHTML(content)[0]);
+    },
     tmpl:function(template, data) {
         var prop, regProp, html = template;
         for (prop in data) regProp = new RegExp("{{" + prop + "}}", "gim"), html = html.replace(regProp, data[prop]);
         return html;
     },
-
     prependNode : function (newNode, referenceNode) {
         referenceNode.insertBefore(newNode, referenceNode.firstChild);
 
     },
-
     createDomNodeFromHTML: function(html){
         var div = document.createElement('div');
         div.innerHTML = html;
         var elements = div.childNodes;
         return elements;
+    },
+
+    addStyle: function(){
+        var head = window.document.getElementsByTagName('head')[0];
+        var style = document.createElement('link');
+        head.appendChild(style);
+        style.setAttribute('rel', 'stylesheet');
+        style.setAttribute('type', 'text/css');
+        style.setAttribute('href', './pages/' + this.options.identifier + '/' + 'style.css');
     }
 
   };
-
   return Renderer;
 })();
