fa263d3f8e42df77dc76c12f2f86bd09746ce3f4
torstendunkel <torsten.dunkel@tamedia.ch>
2017-01-23 14:32:00 +0100

    added grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,16 +8,40 @@ module.exports = function(grunt) {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: 'pages/**/*.css',
-                dest: 'build/'
+                src: 'renderer.js',
+                dest: 'build/renderer.js'
+            }
+        },
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'pages/',
+                    src: ['**/*.css'],
+                    dest: 'build/pages',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['renderer.js', 'pages/ppn_minirectangle1_DE_baslerzeitung/style.css', 'pages/ppn_minirectangle1_DE_baslerzeitung/config.json'],
+                dest: 'dist/built.js'
             }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify','cssmin','concat']);
+    grunt.registerTask('concat', ['concat:dist']);
 
 };
